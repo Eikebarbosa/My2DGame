9007749a08c9178d4c74de0bfa1d423c04ce78b7
Renan <renan.schiavotello05@gmail.com>
2024-05-09 15:14:41 -0300

    ADD: Login

---
+++ src/main/java/login/db/UsuarioDAO.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package login.db;
+
+import login.modelo.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author schix
+ */
+public class UsuarioDAO {
+    
+    public boolean existe(Usuario u) throws Exception{
+        // 1. especificar o comando SQL (Select)
+        String sql = "SELECT * FROM tbusuario_t2 WHERE login = ? AND senha = ?";
+        // 2. estabelecer uma conexão com o banco
+        Connection conexao = new ConnectionFactory(). obterConexao();
+        // 3. Preparar o comando
+        PreparedStatement ps = conexao.prepareStatement(sql);
+        // 4. Substituir os eventuais placeholders
+        ps.setString(1, u.getLogin());
+        ps.setString(2, u.getSenha());
+        // 5. Executar
+        ResultSet rs = ps.executeQuery();
+        // 6. Lidar com o resultado
+        boolean achou = rs.next();
+        // 7. Fechar a conexão
+        rs.close();
+        ps.close();
+        conexao.close();
+        // 8. Responder se existe ou não
+        return achou;
+        
+    }
+}
---
+++ src/main/java/login/modelo/Usuario.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package login.modelo;
+
+/**
+ *
+ * @author schix
+ */
+public class Usuario {
+    // cod, login, senha e tipo
+    private int codigo;
+    private String login;
+    private String senha;
+    private int tipo;
+    
+    //getter
+    public int getTipo(){
+        return tipo;
+    }
+    //setter
+    public void setTipo(int tipo){
+       this.tipo = tipo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+}
