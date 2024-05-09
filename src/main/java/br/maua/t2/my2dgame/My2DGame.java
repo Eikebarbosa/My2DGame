c9660b8d52e1f9d6df0ca2743aecb3dff449055c
Retr0 <eikebarbos@gmail.com>
2024-05-09 14:36:34 -0300

    Initial commit

---
+++ src/main/java/br/maua/t2/my2dgame/My2DGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.maua.t2.my2dgame;
+
+/**
+ *
+ * @author keyex
+ */
+public class My2DGame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
