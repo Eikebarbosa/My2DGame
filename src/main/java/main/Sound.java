f434b317aff0989fc04ff6250deda1d95841311f
Renan <renan.schiavotello05@gmail.com>
2024-06-07 22:16:10 -0300

    UPDATE: Sound

--- src/main/java/main/Sound.java
+++ src/main/java/main/Sound.java
@@ -8,16 +8,17 @@ import java.net.URL;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+
 /**
  *
  * @author Mateus
  */
 public class Sound {
-    
+
     Clip clip;
     URL soundURL[] = new URL[30];
-    
-    public Sound(){
+
+    public Sound() {
         soundURL[0] = getClass().getResource("/sound/BlueBoyAdventure.wav");
         soundURL[1] = getClass().getResource("/sound/coin.wav");
         soundURL[2] = getClass().getResource("/sound/powerup.wav");
@@ -25,26 +26,29 @@ public class Sound {
         soundURL[4] = getClass().getResource("/sound/fanfare.wav");
         soundURL[5] = getClass().getResource("/sound/hitmonster.wav");
         soundURL[6] = getClass().getResource("/sound/receivedamage.wav");
-        
+
     }
-    
-    public void setFile(int i){
-        try{
+
+    public void setFile(int i) {
+        try {
             AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
             clip = AudioSystem.getClip();
             clip.open(ais);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public void play(){
+
+    public void play() {
         clip.start();
     }
-    public void loop(){
+
+    public void loop() {
         clip.loop(Clip.LOOP_CONTINUOUSLY);
     }
-    public void stop(){
+
+    public void stop() {
         clip.stop();
     }
-    
+
 }
